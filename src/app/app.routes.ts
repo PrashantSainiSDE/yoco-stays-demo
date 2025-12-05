983a0f194d6128d838f341b417ba30ccb4d38c27
Prince <prince34998@gmail.com>
2025-12-05 06:54:26 +0530

    feat: add UserList component with routing, HTML, CSS, and test files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './components/user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'user-list', pathMatch: 'full' },
+  {path: 'user-list', component: UserListComponent },
+];
